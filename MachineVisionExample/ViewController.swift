e856efefc7e6708e129954f46265ed7c5d7978b9
Austin Tooley <austin@tooley.me>
2017-06-08 18:08:59 -0500

    Initial Commit

---
+++ MachineVisionExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MachineVisionExample
+//
+//  Created by Austin Tooley on 6/8/17.
+//  Copyright © 2017 Austin Tooley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
